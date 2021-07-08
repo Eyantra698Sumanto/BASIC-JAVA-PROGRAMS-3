52e66df619224e332a2b22b1cc3fac67cb818d7d
Sumanto Kar <jeetsumanto123@gmail.com>
2021-07-08 15:22:15 +0530

    programs

--- basicC/Tut6.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-int a[] = {2,3,4,5,6};
-int i;
-for(i=5; i > 0; i--)
-printf("%d",a[--i]);
-}
--- basicC/Untitled3.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-int a=10,b=2;
-int *pa=&a,*pb=&b;
-printf("value = %d", *pa/ *pb);
-return 0;
-}
